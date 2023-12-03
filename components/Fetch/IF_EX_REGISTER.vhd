6284af00174865a585113056bad8ec696cf1acdb
Ismail-Ramadan-Shaheen <114364925+Ismail-Ramadan-Shaheen@users.noreply.github.com>
2023-12-03 23:04:57 +0200

    IF_EX buffer is completed

---
+++ components/Fetch/IF_EX_REGISTER.vhd
@@ -0,0 +1,40 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+ENTITY IF_EX_REGISTER IS
+    PORT (
+        CLK : IN STD_LOGIC;
+        RESET : IN STD_LOGIC;
+
+        INSTRUCTION : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        PC : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        IMM_EN : IN STD_LOGIC;
+        
+        INSTRUCTION_IF_EX : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        PC_IF_EX : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    );
+END ENTITY IF_EX_REGISTER;
+
+ARCHITECTURE Behavioral OF IF_EX_REGISTER IS
+
+BEGIN
+    PROCESS (CLK, RESET)
+    BEGIN
+        IF RESET = '1' THEN
+            -- Synchronous reset
+            INSTRUCTION_IF_EX <= (OTHERS => '0');
+            PC_IF_EX <= (OTHERS => '0');
+        ELSIF RISING_EDGE(CLK) THEN
+            -- Synchronous behavior
+            IF IMM_EN = '1' THEN
+            INSTRUCTION_IF_EX <= (OTHERS => '0');
+            ELSE
+            INSTRUCTION_IF_EX <= INSTRUCTION;
+            END IF;
+            PC_IF_EX <= PC;
+        END IF;
+    END PROCESS;
+
+END ARCHITECTURE Behavioral;
--- components/Fetch/INSTRUCTIONS_MEMORY.vhd
+++ components/Fetch/INSTRUCTIONS_MEMORY.vhd
@@ -2,16 +2,16 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity instructionMemory is
+entity INSTRUCTIONS_MEMORY is
     port(instruction: in std_logic_vector(15 downto 0);
         pc: in std_logic_vector (31 downto 0);
         inst_imm: out std_logic_vector (15 downto 0);
         clk: in std_logic;
         we: in std_logic
         );
-end entity instructionMemory;
+end entity INSTRUCTIONS_MEMORY;
     
-architecture instructionMemoryArch of instructionMemory is
+architecture INSTRUCTIONS_MEMORY_ARCH of INSTRUCTIONS_MEMORY is
     type inst_array is array(0 to 4095) of std_logic_vector (15 downto 0);
     signal instArray : inst_array;
     signal  i: std_logic_vector (11 downto 0):="000000000000"; 
@@ -21,9 +21,9 @@ begin
         if(rising_edge(clk)) then 
             if we = '1' then
                 instArray(to_integer(unsigned(i)))<= instruction;
-                i <= std_logic_vector(to_unsigned(to_integer(unsigned( i  )) + 1, 12));
+                i <= std_logic_vector(to_unsigned(to_integer(unsigned( i )) + 1, 12));
             else inst_imm <= instArray(to_integer(unsigned(PC)));
             end if;
         end if;
     end process;
-end architecture instructionMemoryArch;
+end architecture INSTRUCTIONS_MEMORY_ARCH;
