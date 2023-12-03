80b9ed0c8684c2947f76ee05e46f1ba1805e68ca
Ahmed Abdelatty <ahmedradalla2001@gmail.com>
2023-12-04 01:48:13 +0200

    completed hdu

--- components/decode/alu_src_2_mux.vhd
+++ components/decode/alu_src_2_mux.vhd
@@ -1,13 +1,13 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-use IEEE.NUMERIC_STD.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
 entity alu_src_2_mux is
     port (
-        rd2, pc : in unsigned(31 downto 0);
-        imm : in unsigned(15 downto 0);
-        alu_src : in unsigned(1 downto 0);
+        rd2, pc   : in  unsigned(31 downto 0);
+        imm       : in  unsigned(15 downto 0);
+        alu_src   : in  unsigned (1 downto 0);
         alu_src_2 : out unsigned(31 downto 0)
     );
 end entity alu_src_2_mux;
@@ -18,11 +18,11 @@ begin
     begin
         case alu_src is
             when "00" =>
-                alu_src_2 <= rd2;
+                alu_src_2 <=             rd2;
             when "01" =>
                 alu_src_2 <= resize(imm, 32);
             when "10" =>
-                alu_src_2 <= pc + 1;
+                alu_src_2 <=          pc + 1;
             when others =>
                 alu_src_2 <= (others => 'X');
         end case;
---
+++ components/decode/hdu.vhd
@@ -0,0 +1,30 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+entity hdu is
+    port (
+        old_dst, cur_ra_one, cur_ra_two                     : in  unsigned(2 downto 0);
+        reg_write_one, mem_read, read_reg_one, read_reg_two : in  unsigned(0 downto 0);
+        stall                                               : out unsigned(0 downto 0)
+    );
+end entity hdu;
+
+architecture ArchHDU of hdu is
+begin
+    process (old_dst, cur_ra_one, cur_ra_two, reg_write_one, mem_read, read_reg_one, read_reg_two) is
+    begin
+        if (reg_write_one = "1" and mem_read = "1") then
+            if (read_reg_one = "1" and cur_ra_one = old_dst) then
+                stall <= "1";
+            elsif (read_reg_two = "1" and cur_ra_two = old_dst) then
+                stall <= "1";
+            else
+                stall <= "0";
+            end if;
+        else
+            stall <= "0";
+        end if;
+    end process;
+end architecture ArchHDU;
