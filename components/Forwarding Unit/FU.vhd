ce152782fe255feeaea11798a59b1618fa9c675b
AhmedOsama198 <ahmed.osama1982002@gmail.com>
2023-12-26 21:18:33 +0200

    feat: updated forwarding unit

--- components/Forwarding Unit/FU.vhd
+++ components/Forwarding Unit/FU.vhd
@@ -14,9 +14,11 @@ ENTITY FU IS
         rdst1_ex_mem : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
         rdst2_ex_mem : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
         wb_src_ex_mem : IN STD_LOGIC_VECTOR (1 DOWNTO 0); -- (ALU - MEM - IMM)
+        ior_ex_mem : IN STD_LOGIC; -- IO READ SIGNAL
         -- Inputs from MEM/WB Register
         reg_w1_mem_wb : IN STD_LOGIC;
         reg_w2_mem_wb : IN STD_LOGIC;
+        ior_mem_wb : IN STD_LOGIC; -- IO READ SIGNAL
         rdst1_mem_wb : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
         rdst2_mem_wb : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
         wb_src_mem_wb : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
@@ -38,25 +40,54 @@ BEGIN
     -- 010: input instruction Rsrc == Rdst1 of (instruction before prev instruction) 
     -- 011: input instruction Rsrc == Rdst2 of (instruction before prev instruction) 
     -- 111: No forwarding
-    rsrc1_d_ex_sel <= "111" WHEN (read_reg_1 = '0') ELSE
-        ---  FORWARDING FROM PREV INSTRUCTION 
-        "000" WHEN ((rsrc1_d_ex = rdst1_ex_mem) AND (reg_w1_ex_mem = '1') AND (wb_src_ex_mem = "00")) ELSE -- ALU_OUT
-        "001" WHEN ((rsrc1_d_ex = rdst2_ex_mem) AND (reg_w2_ex_mem = '1')) ELSE-- ALU_SRC2_EX_MEM
-        ---  FORWARDING FROM INSTRUCTION BEFORE PREV 
-        "010" WHEN ((rsrc1_d_ex = rdst1_mem_wb) AND (reg_w1_mem_wb = '1') AND (wb_src_mem_wb = "00")) ELSE -- ALU OUT
-        "011"WHEN ((rsrc1_d_ex = rdst1_mem_wb) AND (reg_w1_mem_wb = '1') AND (wb_src_mem_wb = "01")) ELSE -- MEM OUT 
-        "100"WHEN ((rsrc1_d_ex = rdst2_mem_wb) AND (reg_w2_mem_wb = '1')) ELSE-- ALU_SRC2_MEM_WB
-        "111"
-        ;
-    -- NO FORWARDING 
-    rsrc2_d_ex_sel <= "111" WHEN (read_reg_2 = '0') ELSE
-        ---  FORWARDING FROM PREV INSTRUCTION 
-        "000" WHEN ((rsrc2_d_ex = rdst1_ex_mem) AND (reg_w1_ex_mem = '1') AND (wb_src_ex_mem = "00")) ELSE -- ALU_OUT
-        "001" WHEN ((rsrc2_d_ex = rdst2_ex_mem) AND (reg_w2_ex_mem = '1')) ELSE -- ALU_SRC2_EX_MEM
-        ---  FORWARDING FROM INSTRUCTION BEFORE PREV 
-        "010" WHEN ((rsrc2_d_ex = rdst1_mem_wb) AND (reg_w1_mem_wb = '1') AND (wb_src_mem_wb = "00")) ELSE -- ALU OUT
-        "011"WHEN ((rsrc2_d_ex = rdst1_mem_wb) AND (reg_w1_mem_wb = '1') AND (wb_src_mem_wb = "01")) ELSE -- MEM OUT
-        "100"WHEN ((rsrc2_d_ex = rdst2_mem_wb) AND (reg_w2_mem_wb = '1')) ELSE -- -- ALU_SRC2_MEM_WB
-        "111";
+    PROCESS (read_reg_1, rsrc1_d_ex, rdst1_ex_mem, reg_w1_ex_mem, wb_src_ex_mem,
+        rdst2_ex_mem, reg_w2_ex_mem, rdst1_mem_wb, reg_w1_mem_wb, wb_src_mem_wb,
+        rdst2_mem_wb, reg_w2_mem_wb, ior_ex_mem, ior_mem_wb)
+    BEGIN
+        IF (read_reg_1 = '0') THEN
+            rsrc1_d_ex_sel <= "000";
+        ELSIF ((rsrc1_d_ex = rdst1_ex_mem) AND (ior_ex_mem = '1') AND (wb_src_ex_mem = "11")) THEN
+            rsrc1_d_ex_sel <= "100"; -- IN PORT IN PREV 
+        ELSIF ((rsrc1_d_ex = rdst1_ex_mem) AND (reg_w1_ex_mem = '1') AND (wb_src_ex_mem = "00")) THEN
+            rsrc1_d_ex_sel <= "001"; -- ALU_OUT
+        ELSIF ((rsrc1_d_ex = rdst2_ex_mem) AND (reg_w2_ex_mem = '1')) THEN
+            rsrc1_d_ex_sel <= "010"; -- ALU_SRC2_EX_MEM
+        ELSIF ((rsrc1_d_ex = rdst1_mem_wb) AND (ior_mem_wb = '1') AND (wb_src_mem_wb = "11")) THEN
+            rsrc1_d_ex_sel <= "110"; -- IN PORT IN BEFORE PREV
+        ELSIF ((rsrc1_d_ex = rdst1_mem_wb) AND (reg_w1_mem_wb = '1') AND (wb_src_mem_wb = "00")) THEN
+            rsrc1_d_ex_sel <= "011"; -- ALU OUT
+        ELSIF ((rsrc1_d_ex = rdst1_mem_wb) AND (reg_w1_mem_wb = '1') AND (wb_src_mem_wb = "01")) THEN
+            rsrc1_d_ex_sel <= "101"; -- MEM OUT
+        ELSIF ((rsrc1_d_ex = rdst2_mem_wb) AND (reg_w2_mem_wb = '1')) THEN
+            rsrc1_d_ex_sel <= "110"; -- ALU_SRC2_MEM_WB
+        ELSE
+            rsrc1_d_ex_sel <= "000";
+        END IF;
+    END PROCESS;
+
+    PROCESS (read_reg_2, rsrc1_d_ex, rdst1_ex_mem, reg_w1_ex_mem, wb_src_ex_mem,
+        rdst2_ex_mem, reg_w2_ex_mem, rdst1_mem_wb, reg_w1_mem_wb, wb_src_mem_wb,
+        rdst2_mem_wb, reg_w2_mem_wb, ior_ex_mem, ior_mem_wb)
+    BEGIN
+        IF (read_reg_2 = '0') THEN
+            rsrc2_d_ex_sel <= "000"; -- NO FORWARDING
+        ELSIF ((rsrc2_d_ex = rdst1_ex_mem) AND (ior_ex_mem = '1') AND (wb_src_ex_mem = "11")) THEN
+            rsrc2_d_ex_sel <= "100"; -- IN PORT IN PREV 
+        ELSIF ((rsrc2_d_ex = rdst1_ex_mem) AND (reg_w1_ex_mem = '1') AND (wb_src_ex_mem = "00")) THEN
+            rsrc2_d_ex_sel <= "001"; -- ALU_OUT
+        ELSIF ((rsrc2_d_ex = rdst2_ex_mem) AND (reg_w2_ex_mem = '1')) THEN
+            rsrc2_d_ex_sel <= "010"; -- ALU_SRC2_EX_MEM
+        ELSIF ((rsrc2_d_ex = rdst1_mem_wb) AND (ior_mem_wb = '1') AND (wb_src_mem_wb = "11")) THEN
+            rsrc2_d_ex_sel <= "110"; -- IN PORT IN BEFORE PREV
+        ELSIF ((rsrc2_d_ex = rdst1_mem_wb) AND (reg_w1_mem_wb = '1') AND (wb_src_mem_wb = "00")) THEN
+            rsrc2_d_ex_sel <= "011"; -- ALU OUT
+        ELSIF ((rsrc2_d_ex = rdst1_mem_wb) AND (reg_w1_mem_wb = '1') AND (wb_src_mem_wb = "01")) THEN
+            rsrc2_d_ex_sel <= "101"; -- MEM OUT
+        ELSIF ((rsrc2_d_ex = rdst2_mem_wb) AND (reg_w2_mem_wb = '1')) THEN
+            rsrc2_d_ex_sel <= "110"; -- ALU_SRC2_MEM_WB
+        ELSE
+            rsrc2_d_ex_sel <= "000";
+        END IF;
+    END PROCESS;
 
 END ARCHITECTURE ArchFU;
